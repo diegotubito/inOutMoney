7850865fdd12495c1cda496c680c86d6fa91e1b1
David Diego Gomez <diegodavid@icloud.com>
2019-02-26 00:48:16 -0300

    no message

---
+++ InOutMoneyFirebase/Sources/Gastos/Rubros/Profile/View/Cells/Date/IOTableViewCellGastoProfileFecha.swift
@@ -0,0 +1,26 @@
+//
+//  IOTableViewCellGastoProfileFecha.swift
+//  InOutMoneyFirebase
+//
+//  Created by David Diego Gomez on 26/2/19.
+//  Copyright © 2019 Gomez David Diego. All rights reserved.
+//
+
+import UIKit
+
+class IOTableViewCellGastoProfileFecha: UITableViewCell {
+
+    @IBOutlet var titleCell: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
