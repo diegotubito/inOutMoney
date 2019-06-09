4b24c321817368fa970ae643def703da68ef52be
David Diego Gomez <diegodavid@icloud.com>
2019-06-09 16:48:04 -0300

    Remove KeyNames from IOAltaGastoModel

--- InOutMoneyFirebase/Sources/Home/AltaGasto/Model/IOAltaGastoModel.swift
+++ InOutMoneyFirebase/Sources/Home/AltaGasto/Model/IOAltaGastoModel.swift
@@ -9,26 +9,14 @@
 import Foundation
 
 class IOAltaGastoModel {
-    var rubroSeleccionado : IORubroManager.Rubro
+    var rubroSeleccionado : IOProjectModel.Rubro
     var cuenta_selected_index : Int?
+    var cuentas : [IOProjectModel.Cuenta]
     
-    init(rubroRecibido: IORubroManager.Rubro) {
+    init(rubroRecibido: IOProjectModel.Rubro, cuentas: [IOProjectModel.Cuenta]) {
         self.rubroSeleccionado = rubroRecibido
+        self.cuentas = cuentas
     }
     
-    struct KeyNames {
-        static let queryByTypeMonthYear = "queryByTypeMonthYear"
-        static let queryByTypeYear      = "queryByTypeYear"
-        static let queryByMonthYear     = "queryByMonthYear"
-        static let queryByYear          = "queryByYear"
-        static let childIDRubro         = "childIDRubro"
-        static let isEnabled            = "isEnabled"
-        static let childIDDebito        = "childIDDebito"
-        static let descripcion          = "descripcion"
-        static let fechaGasto           = "fechaGasto"
-        static let fechaCreacion        = "fechaCreacion"
-        static let importe              = "importe"
-        static let type                 = "type"
-        
-    }
+    
 }
